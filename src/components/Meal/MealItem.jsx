522737bb09a7b23879bf1162523be9dd10cb771d
WiFo26 <lou81804@gmail.com>
2022-09-24 11:27:32 -0500

    add form item meal

--- src/components/Meal/MealItem.jsx
+++ src/components/Meal/MealItem.jsx
@@ -1,9 +1,19 @@
-export const MealItem = ({ name, description, price }) => {
+export const MealItem = ({ id, name, description, price }) => {
+  const labelIdentifier = `amount-${id}`
   return (
-    <li className='border-b-2 border-b-slate-300 pb-3 pt-2'>
-      <h3 className='font-bold'>{name}</h3>
-      <p className='my-[2px] text-sm italic'>{description}</p>
-      <span className='font-bold text-amber-600'>${price}</span>
+    <li className='flex items-center justify-between border-b-2 border-b-slate-300 pb-3 pt-2'>
+      <div>
+        <h3 className='font-bold'>{name}</h3>
+        <p className='my-[2px] text-sm italic'>{description}</p>
+        <span className='font-bold text-amber-600'>${price}</span>
+      </div>
+      <form action='#' className='flex flex-col items-center justify-center'>
+        <div className='mb-2'>
+          <label htmlFor={labelIdentifier} className='mr-3 text-sm font-semibold'>Amount</label>
+          <input type='number' min={1} max={5} id={labelIdentifier} defaultValue={1} className='w-10 rounded border-2 border-solid text-sm' />
+        </div>
+        <button className='rounded-full bg-[#762f12] px-4 py-1 text-sm text-white'>+ Add</button>
+      </form>
     </li>
   )
 }
