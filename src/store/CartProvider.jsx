31933b4863948c46ce58485fe8659757e2cf7ab9
WiFo26 <lou81804@gmail.com>
2022-09-25 13:54:20 -0500

    add cart reducer logic

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,37 @@
+import { useReducer } from 'react'
 import { CartContext } from './cart-context'
 
+const INITIAL_CART_STATE = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'add': {
+      const updatedItems = state.items.concat(action.item)
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount
+      }
+    }
+    default:
+      throw new Error()
+  }
+}
+
 export const CartProvider = ({ children }) => {
-  const addItemHandler = (item) => {}
+  const [state, dispatch] = useReducer(cartReducer, INITIAL_CART_STATE)
+
+  const addItemHandler = (item) => {
+    dispatch({ type: 'add', item })
+  }
   const deleteItemHandler = (id) => {}
 
   const defaultValue = {
-    items: [],
-    totalAmount: 0,
+    items: state.items,
+    totalAmount: state.totalAmount,
     addItem: addItemHandler,
     deleteItem: deleteItemHandler
   }
