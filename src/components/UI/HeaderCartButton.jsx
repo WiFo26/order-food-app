1e8e0f8ab5813fb4e054ee39f556436bb8834004
WiFo26 <lou81804@gmail.com>
2022-09-25 13:19:02 -0500

    add number of items logic

--- src/components/UI/HeaderCartButton.jsx
+++ src/components/UI/HeaderCartButton.jsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react'
+import { CartContext } from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 
 export const HeaderCartButton = ({ toggleModal }) => {
+  const { items } = useContext(CartContext)
+  const numberOfItems = items.reduce((acc, item) => {
+    return acc + item.amount
+  }, 0)
   return (
     <button onClick={toggleModal} className='mr-24 flex items-center justify-around gap-1 rounded-full bg-[#4d1601] px-8 py-2 text-xs font-semibold text-white hover:bg-[#92320c]'>
       <div className='h-4 w-4 text-white'>
         <CartIcon />
       </div>
       <span className='mr-2'>Your Cart</span>
-      <span className='rounded-full bg-[#b94517] px-[0.65rem] py-[0.2rem]'>3</span>
+      <span className='rounded-full bg-[#b94517] px-[0.65rem] py-[0.2rem]'>{numberOfItems}</span>
     </button>
   )
 }
