9da7cc05e9c0d9ad4e1397e55a4d2ffd303eb366
WiFoDev <lou81804@gmail.com>
2022-10-04 23:57:43 -0500

    add fetch meals from firebase

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,7 +2,7 @@ import { Header } from '../UI/Header'
 export const Layout = ({ children, toggleModal }) => {
   return (
     <>
-      <div className='bg-zinc-700'>
+      <div className='h-screen bg-zinc-700'>
         <Header toggleModal={toggleModal} />
         <main className='absolute top-20 grid w-full place-items-center gap-6'>
           {children}
--- src/components/Meal/AvailableMeals.jsx
+++ src/components/Meal/AvailableMeals.jsx
@@ -1,45 +1,53 @@
+import { useState, useEffect } from 'react'
 import { Card } from '../UI/Card'
 import { MealItem } from './MealItem'
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99
-  }
-]
-
 export const AvailableMeals = () => {
+  const [meals, setMeals] = useState([])
+  const [loading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('https://order-food-app-56a2a-default-rtdb.firebaseio.com/meals.json')
+      .then(res => res.json())
+      .then(data => {
+        const mealsFromApi = []
+        for (const key in data) {
+          if (Object.hasOwnProperty.call(data, key)) {
+            mealsFromApi.push({
+              id: key,
+              ...data[key]
+            })
+          }
+        }
+        setIsLoading(false)
+        setMeals(mealsFromApi)
+      })
+      .catch(error => {
+        setIsLoading(false)
+        setError(`Something went wrong: ${error.message}`)
+      })
+  }, [])
+
+  const content = meals.length > 0 &&
+    (
+      <ul>
+        {meals.map(meal => {
+          return (
+            <MealItem key={meal.id} {...meal} />
+          )
+        })}
+      </ul>
+    )
+
   return (
-    <div className='flex w-full justify-center bg-zinc-700'>
+    <div className='flex w-full justify-center'>
       <section className='mb-4 h-full w-2/3'>
         <Card bgColor='bg-white'>
-          <ul>
-            {DUMMY_MEALS.map(meal => {
-              return (
-                <MealItem key={meal.id} {...meal} />
-              )
-            })}
-          </ul>
+          {content}
+          {loading && <h1 className='animate-ping text-center text-2xl font-bold'>Loading...</h1>}
+          {error && !meals.length && <h3 className='text-center text-2xl font-bold text-red-600'>{error}</h3>}
         </Card>
       </section>
     </div>
